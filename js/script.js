085813721d7ac4181aff977efaa60cfc01b17f50
goldwinalec <63246019+goldwinalec@users.noreply.github.com>
2021-02-13 12:14:15 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@ $('.accordion__title').on('click', function(){
     $('.accordion__item').removeClass('accordion__item--active');
     $(this).parent().addClass('accordion__item--active');
 
-})
+});
+//
+
+// блок "нам доверяют"
+$('.clients__item-link').on('click', function(evt){
+    evt.preventDefault();
+    $('clients__item').removeClass('clients__item--hidden');
+    $('.clients__item-link').addClass('clients__item-link--hidden');
+});
+
+
+
+
 
 // плеер
-var videoPlayer = videojs('videoPlayer', {
-    controls: true,
-  autoplay: false,
-  preload: 'none',
-  userActions: {
-      hotkeys: true
-  }
-});
+
